0c1d2db828f2118c55b179f68894ebf93d141e59
gustavowmarques <gustavowmarques@gmail.com>
2025-09-13 23:02:23 +0100

    Remove hardcoded AWS credentials; use env vars

--- config/settings.py
+++ config/settings.py
@@ -179,9 +179,9 @@ USE_S3 = (not DEBUG) and bool(os.getenv("smart-recipe-media-prod"))
 if USE_S3:
     DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
-    AWS_STORAGE_BUCKET_NAME = os.getenv("smart-recipe-media-prod")
-    AWS_S3_REGION_NAME = os.getenv("eu-north-1")   # e.g. "eu-west-1"
-    AWS_ACCESS_KEY_ID = os.getenv("AKIA-REDACTED")
+    AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
+    AWS_S3_REGION_NAME = os.getenv("AWS_S3_REGION_NAME")
+    AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
     AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 
     AWS_QUERYSTRING_AUTH = False      # public, cacheable URLs
